23d8178d45ef000f064cb7d9b78d195a416481f1
Garrett Herb <137188366+gwherb@users.noreply.github.com>
2024-11-13 13:41:45 -0500

    update
    Update for more comprehensive stats and for adding input arguments to the main file

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import asyncio
 from poke_env import AccountConfiguration, ShowdownServerConfiguration
 from poke_env.player import RandomPlayer, SimpleHeuristicsPlayer
 from players import *
+import argparse
 
-async def local():
+async def local(n_battles=1):
 
     # Create Player 1
     LLMagikarp = LoggingPlayer()
@@ -12,9 +13,9 @@ async def local():
     HeuristicsPlayer = SimpleHeuristicsPlayer()
 
     # Start the battle
-    await LLMagikarp.battle_against(HeuristicsPlayer, n_battles=1)
+    await LLMagikarp.battle_against(HeuristicsPlayer, n_battles=n_battles)
 
-async def server():
+async def server(n_challenges=1):
 
     # Create Bot
     player = RandomPlayer(
@@ -22,20 +23,36 @@ async def server():
         server_configuration=ShowdownServerConfiguration,
     )
 
-    await player.send_challenges("LLMagikarp", n_challenges=1)
+    await player.send_challenges("LLMagikarp", n_challenges=n_challenges)
 
-async def ladder():
+async def ladder(n_battles=1):
 
     LLMagikarp = LoggingPlayer(
         account_configuration=AccountConfiguration("gwherb", "Just4Gh!"),
         server_configuration=ShowdownServerConfiguration,
     )
 
-    await LLMagikarp.ladder(1)
+    await LLMagikarp.ladder(n_battles)
 
     for battle in LLMagikarp.battles.values():
         print(battle.rating, battle.opponent_rating)
 
+def main():
+    parser = argparse.ArgumentParser(description="Run a Pokemon Showdown bot")
+    parser.add_argument("--mode", type=str, help="Mode to run the bot in")
+    parser.add_argument("--battle_num", type=int, help="Number of battles to run")
+
+    args = parser.parse_args()
+
+    if args.mode == "local":
+        asyncio.get_event_loop().run_until_complete(local(args.battle_num))
+    elif args.mode == "server":
+        asyncio.get_event_loop().run_until_complete(server(args.battle_num))
+    elif args.mode == "ladder":
+        asyncio.get_event_loop().run_until_complete(ladder(args.battle_num))
+    else:
+        print("Invalid mode")
+
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(local())
+    main()
 
--- stats.py
+++ stats.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import json
 from icecream import ic
 from datetime import datetime
+import argparse
+import re
 
 def get_battle_logs(logs_dir="./logs", start_date=None, end_date=None):
     logs_path = Path(logs_dir)
@@ -26,9 +28,63 @@ def get_battle_logs(logs_dir="./logs", start_date=None, end_date=None):
             ic(f"Error processing {log_file}: {e}")
             continue
 
-if __name__ == "__main__":
-    start_date = "20241001"  # Fixed date format to match parsing
-    end_date = "20241112"
+def get_stats(stats_dict, game_type, start_date, end_date):
+    for battle_log in get_battle_logs(start_date=start_date, end_date=end_date):
+        # Skip if this battle log isn't for the game type we're currently processing
+        if battle_log["metadata"]["game_type"] != game_type:
+            continue
+            
+        prev_switch = False
+        turns = []  # List to store the turns of the match
+
+        stats_dict["games_played"] += 1
+        stats_dict['total_random_moves'] += battle_log["metadata"]["random_move_count"]
+        
+        if battle_log["metadata"]["outcome"] == "win":
+            stats_dict["wins"] += 1
+        elif battle_log["metadata"]["outcome"] == "loss":
+            stats_dict["losses"] += 1
+            last_turn_state = battle_log["turns"][-1]["battle_state"]
+            remaining_pokemon = re.search('Remaining Pokemon: (\d)/', last_turn_state)
+            if int(remaining_pokemon.group(1)) <= 2:
+                stats_dict["close_losses"] += 1
+        else:
+            stats_dict["error_matches"] += 1
+
+        for turn in battle_log["turns"]:
+            if turn["action_type"] == "switch":
+                if prev_switch:
+                    stats_dict["double_switches"] += 1
+                prev_switch = True
+
+                stats_dict["total_switches"] += 1
+                turns.append(0)
+
+            elif turn["action_type"] == "move":
+                stats_dict["total_attacks"] += 1
+                prev_switch = False
+                turns.append(1)
+        
+        total_turns = len(turns)
+        stats_dict["avg_turns"] += total_turns
+
+        stats_dict["avg_late_game_switches"] += sum(turns[round(total_turns * 0.75):])
+    
+    # Calculate averages if games were played
+    if stats_dict["games_played"] > 0:
+        stats_dict["avg_turns"] /= stats_dict["games_played"]
+        stats_dict["avg_late_game_switches"] /= stats_dict["games_played"]
+    
+    return stats_dict
+
+def main(start_date='20241030', end_date='20241113'):
+    parser = argparse.ArgumentParser(description="Get stats from Pokemon Showdown logs")
+    parser.add_argument("--start", type=str, help="Start date for logs")
+    parser.add_argument("--end", type=str, help="End date for logs")
+
+    args = parser.parse_args()
+    start_date = args.start
+    end_date = args.end
 
     local_stats = {
         "games_played": 0,
@@ -38,6 +94,10 @@ if __name__ == "__main__":
         "total_random_moves": 0,
         "total_attacks": 0,
         "total_switches": 0,
+        "double_switches": 0,
+        "close_losses": 0,
+        "avg_turns": 0,
+        "avg_late_game_switches": 0
     }
 
     ladder_stats = {
@@ -48,38 +108,20 @@ if __name__ == "__main__":
         "total_random_moves": 0,
         "total_attacks": 0,
         "total_switches": 0,
+        "double_switches": 0,
+        "close_losses": 0,
+        "avg_turns": 0,
+        "avg_late_game_switches": 0
     }
 
-    for battle_log in get_battle_logs(start_date=start_date, end_date=end_date):
-        if battle_log["metadata"]["game_type"] == "local":
-            local_stats["games_played"] += 1
-            local_stats['total_random_moves'] += battle_log["metadata"]["random_move_count"]
-            
-            if battle_log["metadata"]["outcome"] == "win":
-                local_stats["wins"] += 1
-            elif battle_log["metadata"]["outcome"] == "loss":
-                local_stats["losses"] += 1
-            else:
-                local_stats["error_matches"] += 1
+    # Process each type of game separately
+    local_stats = get_stats(local_stats, "local", start_date, end_date)
+    ladder_stats = get_stats(ladder_stats, "ladder", start_date, end_date)
 
-            for turn in battle_log["turns"]:
-                local_stats["total_attacks"] += 1 if turn["action_type"] == "move" else 0
-                local_stats["total_switches"] += 1 if turn["action_type"] == "switch" else 0
-        elif battle_log["metadata"]["game_type"] == "ladder":
-            ladder_stats["games_played"] += 1
-            ladder_stats['total_random_moves'] += battle_log["metadata"]["random_move_count"]
-            
-            if battle_log["metadata"]["outcome"] == "win":
-                ladder_stats["wins"] += 1
-            elif battle_log["metadata"]["outcome"] == "loss":
-                ladder_stats["losses"] += 1
-            else:
-                ladder_stats["error_matches"] += 1
-
-            for turn in battle_log["turns"]:
-                ladder_stats["total_attacks"] += 1 if turn["action_type"] == "move" else 0
-                ladder_stats["total_switches"] += 1 if turn["action_type"] == "switch" else 0
 
     ic("From {} to {}".format(start_date, end_date))
     ic(local_stats)
     ic(ladder_stats)
+
+if __name__ == "__main__":
+    main()
